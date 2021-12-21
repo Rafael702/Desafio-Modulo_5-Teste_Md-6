b479b23a580a813dc150956847d414446aa32fa5
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-12-21 14:27:19 -0300

    Alterar status para Aguardando toda vez que uma conta for salva.

--- src/main/java/br/com/zup/DesafioModulo5_TesteMd6/conta/ContaService.java
+++ src/main/java/br/com/zup/DesafioModulo5_TesteMd6/conta/ContaService.java
@@ -1,5 +1,6 @@
 package br.com.zup.DesafioModulo5_TesteMd6.conta;
 
+import br.com.zup.DesafioModulo5_TesteMd6.enums.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ public class ContaService {
     private ContaRepository contaRepository;
 
     public Conta salvarConta(Conta conta) {
+        conta.setStatus(Status.AGUARDANDO);
         contaRepository.save(conta);
         return conta;
     }
