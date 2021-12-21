83087d74de2256748b9bd3440f997914db1f10fe
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-12-21 12:09:45 -0300

    Criar repository para a conta.

---
+++ src/main/java/br/com/zup/DesafioModulo5_TesteMd6/conta/ContaRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.DesafioModulo5_TesteMd6.conta;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContaRepository extends CrudRepository<Conta, Integer> {
+
+}
