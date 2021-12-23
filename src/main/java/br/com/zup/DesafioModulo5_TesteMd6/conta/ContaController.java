d12468e9d0e659f0005c607e5f9b817efc0a0daf
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-12-23 11:05:16 -0300

    Refatorar método de exibição de conta.

--- src/main/java/br/com/zup/DesafioModulo5_TesteMd6/conta/ContaController.java
+++ src/main/java/br/com/zup/DesafioModulo5_TesteMd6/conta/ContaController.java
@@ -1,8 +1,13 @@
 package br.com.zup.DesafioModulo5_TesteMd6.conta;
 
+import br.com.zup.DesafioModulo5_TesteMd6.conta.dtos.ContaSaidaDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/contas")
@@ -10,6 +15,9 @@ public class ContaController {
 
     @Autowired
     private ContaService contaService;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @PostMapping("/cadastro")
     @ResponseStatus(HttpStatus.CREATED)
@@ -19,8 +27,15 @@ public class ContaController {
     }
 
     @GetMapping
-    public Iterable<Conta> exibirTodasAsContas() {
-        return contaService.buscarTodasAsContas();
-    }
+    public List<ContaSaidaDTO> exibirTodasAsContas() {
 
+        List<ContaSaidaDTO> listaContaSaidaDTOS = new ArrayList<>();
+
+        for (Conta contaReferencia : contaService.exibirTodasAsContas()) {
+            ContaSaidaDTO contaSaidaDTO = modelMapper.map(contaReferencia, ContaSaidaDTO.class);
+            listaContaSaidaDTOS.add(contaSaidaDTO);
+        }
+
+        return listaContaSaidaDTOS;
+    }
 }
